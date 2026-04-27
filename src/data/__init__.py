8baa8114cb14872e5739ed647b8319d74a8f624d
Azizur Rahman <azizerorahman@stu.scu.edu.cn>
2026-04-27 08:28:34 +0800

    Remove unused __init__.py files and clean up .gitignore for Python cache and logs

--- src/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dataloader import IDRiDTriageDataset, get_dataloaders, get_transforms
-
-__all__ = ['IDRiDTriageDataset', 'get_dataloaders', 'get_transforms']
--- src/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .metrics import MetricsTracker, save_checkpoint, load_checkpoint, save_metrics_json, EarlyStopping
-
-__all__ = ['MetricsTracker', 'save_checkpoint', 'load_checkpoint', 'save_metrics_json', 'EarlyStopping']
